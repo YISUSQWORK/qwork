342974875f4a1b83258b59f1bdb2856f074e7f19
YISUSQWORK <52511545+YISUSQWORK@users.noreply.github.com>
2019-07-03 18:17:31 -0400

    Add files via upload

---
+++ QWORK.js
@@ -0,0 +1,95 @@
+// ==UserScript==
+// @name         QWORK
+// @version      4.0
+// @description  KOLO RC
+// @author       YISUS QWORK
+// @updateURL    https://raw.githubusercontent.com/YISUSQWORK/qwork/master/script.js
+// @downloadURL  https://raw.githubusercontent.com/YISUSQWORK/qwork/master/script.js
+// @noframes
+// Kolo
+// @exclude      *//kolotibablo.com/*
+// Funcaptchas
+// @exclude      https://captcha.kik.com/*
+// @exclude      https://www.easports.com/*
+// Puzzles
+// @exclude      https://bitrabbit.io/*
+// @exclude      https://concerts1.liven/*
+// @exclude      https://mbaex.net/*
+// @exclude      https://p2pb2b.io/*
+// @exclude      https://wftc3.e-travel.com/*
+// @exclude      https://www.binance.com/*
+// @exclude      https://www.idax.pro/*
+// @exclude      https://www.mbaex.com/*
+// @exclude      https://www.ticketmaster.com/*
+// @exclude      https://www1.ticketmaster.com/*
+// Dominios Malos
+// @exclude      https://archers-contois.fr/*
+// @exclude      https://automizy.com/*
+// @exclude      https://dropbox.com/*
+// @exclude      https://forum-wf.de/*
+// @exclude      https://idpassets.uefa.com/*
+// @exclude      https://landscapper.com/*
+// @exclude      https://meduza.io/*
+// @exclude      https://pcrecruiter.net/*
+// @exclude      https://upload.net/*
+// @exclude      https://www.christianmingle.com/*
+// @exclude      https://www.footaction.com/*
+//------------------------------------------------------------
+// @exclude      https://combathutch.com/*
+// @exclude      https://goatscript.com/*
+// @exclude      https://hawkincome.com/*
+// @exclude      https://ibisunhappy.com/*
+// @exclude      https://lakewarrant.com/*
+// @exclude      https://litigationlens.com/*
+// @exclude      https://microwavemega.com/*
+// @exclude      https://musketearl.com/*
+// @exclude      https://mustarddogwood.com/*
+// @exclude      https://radiantaxil.com/*
+// @exclude      https://trunkcream.com/*
+// @exclude      https://wuffpartyhat.com/*
+//----------------------------30/6----------------------------
+// @exclude      https://aideethel.com/*
+// @exclude      https://bayotantex.com/*
+// @exclude      https://bowlshrub.com/*
+// @exclude      https://brilliantamy.com/*
+// @exclude      https://conservatoryleather.com/*
+// @exclude      https://constructopposition.com/*
+// @exclude      https://doghouse.com/*
+// @exclude      https://firstchoice.com/*
+// @exclude      https://healthyfail.com/*
+// @exclude      https://hearingmarch.com/*
+// @exclude      https://herboutgo.com/*
+// @exclude      https://hibiscusmovies.com/*
+// @exclude      https://intelligencesciences.com/*
+// @exclude      https://irrigationfish.com/*
+// @exclude      https://jackdawwharf.com/*
+// @exclude      https://managedemail.com/
+// @exclude      https://marshmallow.com/*
+// @exclude      https://narcissusparboil.com/*
+// @exclude      https://palaceduke.com/*
+// @exclude      https://partnersoutdoor.com/*
+// @exclude      https://ranunculusboxer.com/*
+// @exclude      https://regaloleander.com/*
+// @exclude      https://spatulauniverse.com/*
+// @exclude      https://sweetpotato.com/*
+// @exclude      https://vecateliev.com/*
+//-----------------------------1/7----------------------------
+// @exclude      https://appletdungeon.com/*
+// @exclude      https://brazilsick.com/*
+// @exclude      https://exitpollgill.com/*
+// @exclude      https://explorationrealm.com/*
+// @exclude      https://humidifierbattle.com/*
+// @exclude      https://inventquetzal.com/*
+// @exclude      https://moderndrawers.com/*
+// @exclude      https://rillflywheel.com/*
+// @exclude      https://saltmanager.com/*
+// @exclude      https://scalescrub.com/*
+// @exclude      https://sideewalk.com/*
+// @exclude      https://stalkinventor.com/*
+//-----------------------------3/7----------------------------
+// @include      *
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+document.documentElement.innerHTML += ("<embed width=100% src='https://AGREGAR PAGINA'</embed>");
